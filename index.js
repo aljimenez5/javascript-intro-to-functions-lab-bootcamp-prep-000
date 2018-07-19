06e3e5f6a3154767228af8f19330e634aae4d143
arvida.jimenez@gmail.com <arvida.jimenez@gmail.com>
2018-07-19 14:07:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === logWhisper(string)) {
+  if (string === whisper(string)) {
     return "I can\'t hear you!"
   }
-  else if (string === logShout(string)) {
+  else if (string === shout(string)) {
       return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
